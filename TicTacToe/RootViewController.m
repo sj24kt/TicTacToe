9944fc6864d3d51c96f6752aa1061804fab03c58
Sherrie Jones <sj24kt@comcast.net>
2015-03-12 14:31:35 -0600

    Create Storyboard & autolayout

---
+++ TicTacToe/RootViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TicTacToe
+//
+//  Created by Sherrie Jones on 3/12/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
